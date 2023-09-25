33d27da3ecfed8d6b266dfd3cbc90dbc0e3ab9a9
Felix <felixgomezenriquez@gmail.com>
2023-09-25 13:11:15 +0200

    added func changeAnswers and

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
-import { Question } from 'src/app/interfaces/question';
+import confetti from 'canvas-confetti';
+import { TriviaService } from 'src/app/services/trivia.service';
+import { Component, Input, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Game } from 'src/app/interfaces/game';
+import { TimerComponent } from '../timer/timer.component';
+import { Category } from 'src/app/interfaces/category';
 
 @Component({
   selector: 'app-question',
@@ -7,31 +12,55 @@ import { Question } from 'src/app/interfaces/question';
   styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent {
-  @Input() question?: Question;
+  @Input() game!: Game;
+  @Input() categorySelected!: Category;
+
+  @ViewChild(TimerComponent) timerComponent?: TimerComponent; // Obtén una referencia al componente hijo
 
   answers: string[] = [];
 
+  constructor(private router: Router, private triviaService: TriviaService) {}
   ngOnInit(): void {
-    // console.table(this.question!);
-
-    // console.log(this.question?.results[0]);
-    // this.question?.results[0].category;
-    // console.log(this.question?.results);
-    // console.log(this.question?.results[0].correct_answer);
-    // console.log(this.question?.results[0].incorrect_answers);
-    let answers:string[]=[];
-
-    answers.push(this.question!.results[0].correct_answer);
-    answers = answers.concat(this.question!.results[0].incorrect_answers);
-    //Desordenamos las respuestas
-    answers.sort(() => {
-      return Math.random() - 0.5;
-    });
-
-    this.answers= answers;
-
+    this.answers=this.changeAnswers()
   }
 
+  changeAnswers():string[]{
+     let answers: string[] = [];
+
+     answers.push(this.game.currentQuestion!.results[0].correct_answer);
+     answers = answers.concat(
+       this.game.currentQuestion!.results[0].incorrect_answers
+     );
+     //Desordenamos las respuestas
+     answers.sort(() => {
+       return Math.random() - 0.5;
+     });
+
+     return answers;
+  }
+  ngAfterViewInit() {
+    console.log();
+  }
+  async checkAnswer(answer: HTMLElement): Promise<void> {
+    console.log(this.timerComponent);
+
+    let isCorrect =
+      answer.innerHTML == this.game.currentQuestion!.results[0].correct_answer;
+
+      console.log(isCorrect);
+    if (isCorrect) {
+      this.game.askedQuestions++;
+      this.game.correctAnswers++;
+      this.timerComponent?.resetTimer();
+      this.game.currentQuestion = await this.triviaService
+        .getQuestionEasy(this.categorySelected, this.game.apiKey)
+        .toPromise();
+      this.answers = this.changeAnswers();
+      confetti();
+    } else {
+      this.game.askedQuestions++;
+      this.router.navigate(['/fail',this.game] );
+    }
+  }
   ngOnChanges(): void {}
 }
-
