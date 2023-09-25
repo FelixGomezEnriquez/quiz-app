946231a32abcd290aae1c3f065cafb11da29d15d
Felix <felixgomezenriquez@gmail.com>
2023-09-25 09:47:27 +0200

    fix tipo de question

--- src/app/interfaces/question.ts
+++ src/app/interfaces/question.ts
@@ -1,4 +1,8 @@
 export interface Question {
-  response_code: number;
-  results: [];
+  results: [{
+    category: string;
+    correct_answer: string;
+    incorrect_answers: string[];
+    question: string;
+  }];
 }
