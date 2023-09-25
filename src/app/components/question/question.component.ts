10db8fdae9efd261d7d6d41477a53951784ef835
Felix <felixgomezenriquez@gmail.com>
2023-09-25 09:46:44 +0200

    implementado desorden de preguntas y diseño basico de la pregunta

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,20 +1,37 @@
 import { Component, Input } from '@angular/core';
+import { Question } from 'src/app/interfaces/question';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent {
+  @Input() question?: Question;
 
-  @Input() question: any ;
-
+  answers: string[] = [];
 
   ngOnInit(): void {
-    console.log(this.question);
+    // console.table(this.question!);
+
+    // console.log(this.question?.results[0]);
+    // this.question?.results[0].category;
+    // console.log(this.question?.results);
+    // console.log(this.question?.results[0].correct_answer);
+    // console.log(this.question?.results[0].incorrect_answers);
+    let answers:string[]=[];
+
+    answers.push(this.question!.results[0].correct_answer);
+    answers = answers.concat(this.question!.results[0].incorrect_answers);
+    //Desordenamos las respuestas
+    answers.sort(() => {
+      return Math.random() - 0.5;
+    });
+
+    this.answers= answers;
+
   }
 
-  ngOnChanges(): void {
-    console.log(this.question);
-  }
+  ngOnChanges(): void {}
 }
+
